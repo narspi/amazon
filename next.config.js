6b78247be8db7639bd696e4aa3e0a125de8db79d
narspi <ivanovmichurina@gmail.com>
2022-05-14 00:39:52 +0300

    stripe commit

--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ module.exports = {
             'links.papareact.com',
             'fakestoreapi.com'
         ]
+    },
+    env: {
+        stripe_public_key: process.env.STRIPE_PUBLIC_KEY
     }
 }
--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -6,11 +6,30 @@ import { useSelector } from "react-redux";
 import { selectItems,selectTotal } from "../slices/basketSlice";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Currency from "react-currency-formatter";
+import { loadStripe } from '@stripe/stripe-js';
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 const Checkout = () => {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const { data: session } = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+    const checkoutSession = await axios.post('/api/create-checkout-session',{
+      items: items,
+      email: session.user.email
+    });
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id
+    })
+
+    if (result.error) {
+      alert(result.error.message)
+    }
+  }
   return (
     <div className="bg-gray-100">
       <Header />
@@ -42,6 +61,8 @@ const Checkout = () => {
                 <span className="font-bold"> <Currency quantity={total}/></span>
               </h2>
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
